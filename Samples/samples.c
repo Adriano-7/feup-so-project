cfbafa4df34155b81dcda7c2dabb1a9708876cc6
Adriano Machado <adriano.alexandre.machado1@gmail.com>
2022-11-09 11:55:15 +0000

    First commit

---
+++ Samples/samples.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]){
+        if (argc <= 3) {
+        fprintf(stderr, "Failed to read m and n");
+        return EXIT_FAILURE;
+        }
+
+        int fd = open(quote.txt, "r");
+
+
+}

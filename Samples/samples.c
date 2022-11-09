dde47fcc4b5a0adeddd50159e57ab808a6903f0a
Adriano Machado <adriano.alexandre.machado1@gmail.com>
2022-11-09 12:51:20 +0000

    Got samples.c to work

--- Samples/samples.c
+++ Samples/samples.c
@@ -1,13 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+int numBytesInFile(){
+        //count number of bytes in quote.txt
+        FILE *fp;
+        fp = fopen("quote.txt", "r");
+        int numBytes = 0;
+        char c;
+        while((c = fgetc(fp)) != EOF){
+                numBytes++;
+        }
+        return numBytes;
+}
 
 int main(int argc, char* argv[]){
-        if (argc <= 3) {
-        fprintf(stderr, "Failed to read m and n");
-        return EXIT_FAILURE;
+        /* check if exactly 2 arguments are present */
+        if (argc <= 2) {
+                printf("You need to give at least two arguments\n");
+                return EXIT_FAILURE;
         }
 
-        int fd = open(quote.txt, "r");
+        //Opening the file quote.txt
+        FILE *fp;
+        fp = fopen("quote.txt", "r");
 
+        //Checking if the file was opened correctly
+        if(fp == NULL){
+                printf("Error opening the file");
+                return EXIT_FAILURE;
+        }
+
+        //argv[1] to int
+        int n = atoi(argv[1]);
+        int m = atoi(argv[2]);
+
+        //Finding the number of bytes in the file
+        int sz = numBytesInFile();
+
+        //Generate n random numbers
+        int rand_pos [n];
+        for(int i=0 ; i<n ; i++){
+                double r = rand()%sz;
+                rand_pos[i] = r;
+        }
+
+
+        //Using fseek to output n number of strings of m bytes
+        for(int i=0 ; i<n ; i++){
+                char buffer[m];
+                fseek(fp, rand_pos[i], SEEK_SET);
+                fread(buffer, sizeof(char), m, fp);
+                //output content from buffer to terminal
+                printf("%s,  ", buffer);
+        }
+        return 0;
 
 }

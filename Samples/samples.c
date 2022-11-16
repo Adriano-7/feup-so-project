27ad005cd17f7822e23308825fcc893b2347539e
Adriano Machado <adriano.alexandre.machado1@gmail.com>
2022-11-16 11:37:47 +0000

    Fixed issues #1 and #2, it now ignores \n and reads unlimited size buffers

--- Samples/samples.c
+++ Samples/samples.c
@@ -4,60 +4,54 @@
 #include <time.h>
 
 int numBytesInFile(){
-        //count number of bytes in quote.txt
-        FILE *fp;
-        fp = fopen("quote.txt", "r");
-        int numBytes = 0;
+        FILE *file;
+        file = fopen("quote.txt", "r");
+        int nBytes = 0;
         char c;
-        while((c = fgetc(fp)) != EOF){
-                numBytes++;
-        }
-        return numBytes;
+        while((c = fgetc(file)) != EOF){nBytes++;}
+        return nBytes;
 }
 
 int main(int argc, char* argv[]){
-        /* check if exactly 2 arguments are present */
+        /* check if exactly m and n */
         if (argc <= 2) {
-                printf("You need to give at least two arguments\n");
+                printf("You need two insert two arguments (n-fragments & m-character)\n");
                 return EXIT_FAILURE;
         }
-
+        int n = atoi(argv[1]); //Transform arguments to int
+        int m = atoi(argv[2]);
 
         //Opening the file quote.txt
-        FILE *fp;
-        fp = fopen("quote.txt", "r");
-
+        FILE *file;
+        file = fopen("quote.txt", "r");
         //Checking if the file was opened correctly
-        if(fp == NULL){
-                printf("Error opening the file");
+        if(file == NULL){
+                printf("Couldn't open the file\n");
                 return EXIT_FAILURE;
         }
-
-        //argv[1] to int
-        int n = atoi(argv[1]);
-        int m = atoi(argv[2]);
 
         //Finding the number of bytes in the file
         int sz = numBytesInFile();
 
-        //Generate n random numbers
+        srand(time(NULL)); //Creating a sub-random sid
 
-
-        int rand_pos [n];
-        srand(time(NULL));
+        int rand_pos [n];//Array to store random positions
         for(int i=0 ; i<n ; i++){
-                double r = rand()%sz;
+                double r = rand()%(sz-m); //generate a random position to star readin
                 rand_pos[i] = r;
         }
 
-
         //Using fseek to output n number of strings of m bytes
         for(int i=0 ; i<n ; i++){
-                char buffer[m];
-                fseek(fp, rand_pos[i], SEEK_SET);
-                fread(buffer, sizeof(char), m, fp);
-                //output content from buffer to terminal
-                printf(">%s<\n", buffer);
+                char buffer[m]; //Buffer where to store the string
+                fseek(file, rand_pos[i], SEEK_SET); //fseek is setting the file position of the stream to the random offset, SEEK_SET means the of offset is calculated from the beggining of the file
+                fread(buffer, sizeof(char), m, file); //reads m bytes of data from file into the buffer
+                for(int j=0 ; j<m ; j++){
+                        if(buffer[j] == '\n'){
+                                buffer[j] = ' ';
+                        }}
+                printf(">%.*s<\n",m,buffer);
         }
+        fclose(file);
         return 0;
 }

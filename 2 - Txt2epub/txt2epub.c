b412ee0e2cc4b437825982d2056492a9b50c6d62
Adriano-7 <adriano.alexandre.machado1@gmail.com>
2022-12-01 21:33:47 +0000

    Solved issue, now i've allocated memory

--- 2 - Txt2epub/txt2epub.c
+++ 2 - Txt2epub/txt2epub.c
@@ -16,15 +16,15 @@ int main(int argc, char* argv[]){
     int pid = 0; //Process id
     int status = 0; //Status of the process
 
-    char* zip [argc + 3]; 
-    zip[0] = "zip"; zip[1] = "ebooks.zip"; zip[2] = "-qq"; zip[argc+1] = NULL;
+    char* zipArguments = malloc(16*n*sizeof(char));
+    zipArguments[0] = "zip"; zipArguments[1] = "ebooks.zip"; zipArguments[2] = "-qq"; zipArguments[argc+1] = NULL;
 
     for(int i=0 ; i<n ; i++){
         char* outputName = malloc(strlen(argv[i+1]) + 5);
         strcpy(outputName, argv[i+1]);
         outputName[strlen(argv[i+1]) - 4] = '\0';
         strcat(outputName, ".epub");
-        zip[i+3] = outputName;
+        zipArguments[i+3] = outputName;
 
         pid = fork();
         if(pid == 0){
@@ -43,6 +43,7 @@ int main(int argc, char* argv[]){
         }
     }
 
-    if(execvp("zip", zip)<0){ printf("Error in execvp zipping .epub\n"); return EXIT_FAILURE;}
+    if(execvp("zip", zipArguments)<0){ printf("Error in execvp while zipping"); return EXIT_FAILURE;}
+    free(zipArguments);
     return EXIT_SUCCESS;
 }
